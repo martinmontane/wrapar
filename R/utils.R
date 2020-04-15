80f1f868ed8cb091c0ca7c0df4ccfb493d360c51
martinmontane <martinmontane@gmail.com>
2020-04-14 22:08:06 -0300

    Corrección en la función de geolocalización

--- R/utils.R
+++ R/utils.R
@@ -7,10 +7,10 @@
 checkInternet <- function(){
   attempt::stop_if_not(.x = curl::has_internet(),msg = "Error: no se detecta conexión a internet.")
 }
-checkResponseStatus <- function(response){
-  attempt::stop_if_not(.x=response$status_code,
-                       .p = ~ .x == 200,
-                       msg = "Error: hubo algún problema en la respuesta de la API")
-}
-
+# checkResponseStatus <- function(response){
+#   attempt::stop_if_not(.x=response$status_code,
+#                        .p = ~ .x == 200,
+#                        msg = "Error: hubo algún problema en la respuesta de la API")
+# }
+#
 
